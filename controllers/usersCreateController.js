55c6dd0ff0bbaa61a04e21b09b5576dc8ce3ff05
Harris Miller <harrismiller08@gmail.com>
2014-06-07 03:26:00 -0600

    improved userCreateController

--- controllers/usersCreateController.js
+++ controllers/usersCreateController.js
@@ -1,14 +1,9 @@
 App.UsersCreateController = Ember.ObjectController.extend({
 	actions: {
 		save: function() {
-			var newUser = this.store.createRecord('user', {
-				avatarUrl: this.get('avatarUrl'),
-				firstName: this.get('firstName'),
-				lastName: this.get('lastName'),
-				email: this.get('email'),
-				bio: this.get('bio'),
-				creationDate: new Date()
-			});
+			var data = this.getProperties('avatarUrl', 'firstName', 'lastName', 'email', 'bio');
+			var newUser = this.store.createRecord('user', data);
+			newUser.set('creationDate', new Date());
 			newUser.save();
 
 			this.transitionToRoute('user', newUser);
